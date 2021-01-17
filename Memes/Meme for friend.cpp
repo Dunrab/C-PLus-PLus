0955bd467efa3b970d6f831c65e84c52665e4cb3
Dunrab <72047814+Dunrab@users.noreply.github.com>
2021-01-16 23:10:33 -0500

    written for my friend for memes

---
+++ Memes/Meme for friend.cpp
@@ -0,0 +1,380 @@
+#include<vector>
+#include<iostream>
+#include <string>
+#include <iterator>
+#include <unistd.h>
+#include <cstdlib>
+#include <stdlib.h> 
+#include <windows.h>
+#include <fstream>
+#include "time.h"
+using namespace std;
+
+//function for download speed will go here
+
+int main()
+{
+	vector<string> pornNames;
+	pornNames.push_back("Fast and Furious in Bed.mp4");
+	pornNames.push_back("Milf anal fisting.mp4");
+	pornNames.push_back("Shemale ball gag hot.mp4");
+	pornNames.push_back("Stunning teen foot job.mp4");
+	pornNames.push_back("Victoria BBC Gang Bang.mp4");
+	pornNames.push_back("Tight asain girl fisted.mp4");
+	pornNames.push_back("POV piss blowjob.mp4");
+	pornNames.push_back("Andrea takes on mandingo.mp4");
+	pornNames.push_back("Midget Cuck Fest.wav");
+	pornNames.push_back("The Great Midget Migration of 2019.wav");
+	pornNames.push_back("Splash Zone.wav");
+	pornNames.push_back("Welcome to cummer town.mp5");
+	pornNames.push_back("Kingdom of Cunnalingus?.mp5");
+	pornNames.push_back("Harthen's Sex Tape With Papi (8bit 480p).xml");
+	pornNames.push_back("Sam's Spoon Adventures at the Salty Speluge.mp4");
+	pornNames.push_back("Benis.exe");
+	pornNames.push_back("I like them big.jpeg");
+	pornNames.push_back("I likem chunky.png");
+	pornNames.push_back("Nastyboy FT. Elli.mp4");
+	
+	/*
+		Add a function to create the acutal files with their proper names and extensions.
+	*/
+	
+	int i;
+	while (i < 0)//change this numer if set to 100, it may crash a low end PC!!!!!
+	{
+		//To open all the caltulactors you need to have only one in use. The other slots will be used for other windwos programs.
+		//Paint, cmd, photos do not work.
+		WinExec("calc", 1);
+		WinExec("explorer", 1);
+		WinExec("notepad", 1);
+		i++;
+	}
+	
+	
+	//creates a folder to add the files too.
+	system("mkdir C:\\homework");
+	sleep(2);	
+	//will create a file in a specific directory.
+    std::ofstream of2( "C:\\homework\\Milf anal fisting.mp4" );
+    std::ofstream of3( "C:\\homework\\Shemale ball gag hot.mp4" );
+    std::ofstream of4( "C:\\homework\\Stunning teen foot job.mp4" );
+    std::ofstream of5( "C:\\homework\\Victoria BBC Gang Bang.mp4" );
+    std::ofstream of6( "C:\\homework\\Tight asain girl fisted.mp4" );
+    std::ofstream of7( "C:\\homework\\POV piss blowjob.mp4.mp4" );
+    std::ofstream of8( "C:\\homework\\Andrea takes on mandingo.mp4.mp4" );
+    std::ofstream of9( "C:\\homework\\Midget Cuck Fest.wav.mp4" );
+    std::ofstream of10( "C:\\homework\\The Great Midget Migration of 2019.wav");
+    std::ofstream of12( "C:\\homework\\Splash Zone.wav.mp4" );
+    std::ofstream of13( "C:\\homework\\Welcome to cummer town.mp5" );
+    std::ofstream of14( "C:\\homework\\Kingdom of Cunnalingus?.mp5" );
+    std::ofstream of15("C:\\homework\\Harthen's Sex Tape With Papi (8bit 480p).xml.mp4");
+    std::ofstream of16("C:\\homework\\Sam's Spoon Adventures at the Salty Speluge.mp4");
+    //new file outputs for porn names
+	std::ofstream of17("C:\\homework\\Benis.exe");
+	std::ofstream of18("C:\\homework\\I like them big.jpeg");
+	std::ofstream of19("C:\\homework\\I like them chunky.png");
+	std::ofstream of20("C:\\homework\\Nastyboy FT. Elli.mp4");
+	
+	
+	system("dir");
+	system("DATE");
+	
+	/* note to self:
+	// add a print function to print to add download speed to the print outs
+	//
+	*/
+	//raandom download speed not finished yet
+	float v1;
+	v1 = rand() % 100;
+	cout << v1<<endl;
+	
+	while(1)
+{
+	cout<<"Downloding this current porn: ";
+	cout<<pornNames[0];
+    cout<<endl;
+    sleep(2);
+    cout<<"Download Progress : ======                         :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============                   :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ==================             :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ========================       :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============================== :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ===============================:"<<endl;
+    sleep(2);
+	cout<<"Download Complete!  "<<endl;
+	cout<<"Saving too the following directory: C:/PornStash/ "<<endl;
+	
+		cout<<"Downloding this current porn: ";
+	cout<<pornNames[1];
+    cout<<endl;
+    sleep(2);
+    cout<<"Download Progress : ======                         :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============                   :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ==================             :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ========================       :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============================== :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ===============================:"<<endl;
+    sleep(2);
+	cout<<"Download Complete!  "<<endl;
+	cout<<"Saving too the following directory: C:/PornStash/ "<<endl;
+	
+		cout<<"Downloding this current porn: ";
+	cout<<pornNames[2];
+    cout<<endl;
+    sleep(2);
+    cout<<"Download Progress : ======                         :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============                   :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ==================             :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ========================       :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============================== :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ===============================:"<<endl;
+    sleep(2);
+	cout<<"Download Complete!  "<<endl;
+	cout<<"Saving too the following directory: C:/PornStash/ "<<endl;
+	
+		cout<<"Downloding this current porn: ";
+	cout<<pornNames[3];
+    cout<<endl;
+    sleep(2);
+    cout<<"Download Progress : ======                         :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============                   :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ==================             :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ========================       :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============================== :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ===============================:"<<endl;
+    sleep(2);
+	cout<<"Download Complete!  "<<endl;
+	cout<<"Saving too the following directory: C:/PornStash/ "<<endl;
+	
+		cout<<"Downloding this current porn: ";
+	cout<<pornNames[4];
+    cout<<endl;
+    sleep(2);
+    cout<<"Download Progress : ======                         :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============                   :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ==================             :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ========================       :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============================== :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ===============================:"<<endl;
+    sleep(2);
+	cout<<"Download Complete!  "<<endl;
+	cout<<"Saving too the following directory: C:/PornStash/ "<<endl;
+    
+    
+    	cout<<"Downloding this current porn: ";
+	cout<<pornNames[5];
+    cout<<endl;
+    sleep(2);
+    cout<<"Download Progress : ======                         :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============                   :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ==================             :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ========================       :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============================== :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ===============================:"<<endl;
+    sleep(2);
+	cout<<"Download Complete!  "<<endl;
+	cout<<"Saving too the following directory: C:/PornStash/ "<<endl;
+	
+		cout<<"Downloding this current porn: ";
+	cout<<pornNames[6];
+    cout<<endl;
+    sleep(2);
+    cout<<"Download Progress : ======                         :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============                   :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ==================             :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ========================       :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============================== :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ===============================:"<<endl;
+    sleep(2);
+	cout<<"Download Complete!  "<<endl;
+	cout<<"Saving too the following directory: C:/PornStash/ "<<endl;
+	
+		cout<<"Downloding this current porn: ";
+	cout<<pornNames[7];
+    cout<<endl;
+    sleep(2);
+    cout<<"Download Progress : ======                         :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============                   :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ==================             :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ========================       :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============================== :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ===============================:"<<endl;
+    sleep(2);
+	cout<<"Download Complete!  "<<endl;
+	cout<<"Saving too the following directory: C:/PornStash/ "<<endl;
+	
+		cout<<"Downloding this current porn: ";
+	cout<<pornNames[8];
+    cout<<endl;
+    sleep(2);
+    cout<<"Download Progress : ======                         :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============                   :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ==================             :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ========================       :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============================== :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ===============================:"<<endl;
+    sleep(2);
+	cout<<"Download Complete!  "<<endl;
+	cout<<"Saving too the following directory: C:/PornStash/ "<<endl;
+	
+		cout<<"Downloding this current porn: ";
+	cout<<pornNames[9];
+    cout<<endl;
+    sleep(2);
+    cout<<"Download Progress : ======                         :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============                   :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ==================             :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ========================       :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============================== :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ===============================:"<<endl;
+    sleep(2);
+	cout<<"Download Complete!  "<<endl;
+	cout<<"Saving too the following directory: C:/PornStash/ "<<endl;
+	
+		cout<<"Downloding this current porn: ";
+	cout<<pornNames[10];
+    cout<<endl;
+    sleep(2);
+    cout<<"Download Progress : ======                         :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============                   :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ==================             :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ========================       :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============================== :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ===============================:"<<endl;
+    sleep(2);
+	cout<<"Download Complete!  "<<endl;
+	cout<<"Saving too the following directory: C:/PornStash/ "<<endl;
+	
+		cout<<"Downloding this current porn: ";
+	cout<<pornNames[11];
+    cout<<endl;
+    sleep(2);
+    cout<<"Download Progress : ======                         :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============                   :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ==================             :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ========================       :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============================== :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ===============================:"<<endl;
+    sleep(2);
+	cout<<"Download Complete!  "<<endl;
+	cout<<"Saving too the following directory: C:/PornStash/ "<<endl;
+	
+		cout<<"Downloding this current porn: ";
+	cout<<pornNames[12];
+    cout<<endl;
+    sleep(2);
+    cout<<"Download Progress : ======                         :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============                   :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ==================             :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ========================       :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============================== :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ===============================:"<<endl;
+    sleep(2);
+	cout<<"Download Complete!  "<<endl;
+	cout<<"Saving too the following directory: C:/PornStash/ "<<endl;
+	
+		cout<<"Downloding this current porn: ";
+	cout<<pornNames[13];
+    cout<<endl;
+    sleep(2);
+    cout<<"Download Progress : ======                         :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============                   :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ==================             :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ========================       :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============================== :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ===============================:"<<endl;
+    sleep(2);
+	cout<<"Download Complete!  "<<endl;
+	cout<<"Saving too the following directory: C:/PornStash/ "<<endl;
+	
+		cout<<"Downloding this current porn: ";
+	cout<<pornNames[14];
+    cout<<endl;
+    sleep(2);
+    cout<<"Download Progress : ======                         :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============                   :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ==================             :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ========================       :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ============================== :"<<endl;
+    sleep(2);
+	cout<<"Download Progress : ===============================:"<<endl;
+    sleep(2);
+	cout<<"Download Complete!  "<<endl;
+	cout<<"Saving too the following directory: C:/PornStash/ "<<endl;
+	
+	}//end of infitne while loop
+}//your welcome Anthony *muah*.
